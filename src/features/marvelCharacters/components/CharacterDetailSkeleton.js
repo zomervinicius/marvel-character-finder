623404744aa011af3eb370b07cdfb9a3f13d7563
zomervinicius <vinicius.zomer123@gmail.com>
2020-04-28 20:07:04 -0300

    separate character detail skeleton component

---
+++ src/features/marvelCharacters/components/CharacterDetailSkeleton.js
@@ -0,0 +1,72 @@
+import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import Skeleton from '@material-ui/lab/Skeleton'
+import React from 'react'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '90vh'
+  }
+}))
+
+export function CharacterDetailSkeleton() {
+  const classes = useStyles()
+  const theme = useTheme()
+  const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down('xs'))
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <Grid item xs={false} sm={6} md={7}>
+        <Skeleton
+          variant="rect"
+          style={{
+            paddingTop: '80%'
+          }}
+          width="auto"
+          height="auto"
+          animation="wave"
+        />
+      </Grid>
+      {isExtraSmallScreen && (
+        <Grid
+          item
+          xs={12}
+          sm={6}
+          md={3}
+          style={{
+            marginTop: '40px'
+          }}
+        >
+          <Skeleton
+            animation="wave"
+            style={{
+              marginBottom: '8px'
+            }}
+          />
+          <Skeleton
+            variant="rect"
+            style={{
+              paddingTop: '80%'
+            }}
+            width="auto"
+            height="auto"
+            animation="wave"
+          />
+        </Grid>
+      )}
+      <Grid item xs={12} sm={6} md={5} component={Paper} elevation={6} square>
+        {[...Array(33)].map((i) => (
+          <Skeleton
+            animation="wave"
+            key={i}
+            style={{
+              marginTop: '8px'
+            }}
+          />
+        ))}
+      </Grid>
+    </Grid>
+  )
+}
--- src/features/marvelCharacters/pages/CharacterDetail.js
+++ src/features/marvelCharacters/pages/CharacterDetail.js
@@ -1,29 +1,16 @@
-import Grid from '@material-ui/core/Grid'
-import Paper from '@material-ui/core/Paper'
-import { makeStyles, useTheme } from '@material-ui/core/styles'
-import useMediaQuery from '@material-ui/core/useMediaQuery'
-import Skeleton from '@material-ui/lab/Skeleton'
 import apiErrorImage from 'assets/images/marvel-api-error.jpg'
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useHistory } from 'react-router-dom'
 import { CharacterDetailInfo } from '../components/CharacterDetailInfo'
+import { CharacterDetailSkeleton } from '../components/CharacterDetailSkeleton'
 import { CharactersError } from '../components/CharactersError'
 import { fetchCharacterById, selectCharacter } from '../slices/CharacterSlice'
 import { isObjEmpty, scrollToTop } from '../utilities'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minHeight: '90vh'
-  }
-}))
-
 export default function CharacterDetail() {
-  const classes = useStyles()
   const dispatch = useDispatch()
   const history = useHistory()
-  const theme = useTheme()
-  const isExtraSmallScreen = useMediaQuery(theme.breakpoints.down('xs'))
   const { entities: character, loading, error } = useSelector(selectCharacter)
   const getCharacterIdInURL = () => {
     const splittedPathname = history.location.pathname.split('/detail/')
@@ -54,36 +41,7 @@ export default function CharacterDetail() {
       loading === 'idle') ||
     loading === 'pending'
   ) {
-    return (
-      <Grid container component="main" className={classes.root}>
-        <Grid item xs={false} sm={6} md={7}>
-          <Skeleton
-            variant="rect"
-            style={{ paddingTop: '80%' }}
-            width="auto"
-            height="auto"
-            animation="wave"
-          />
-        </Grid>
-        {isExtraSmallScreen && (
-          <Grid item xs={12} sm={6} md={3} style={{ marginTop: '40px' }}>
-            <Skeleton animation="wave" style={{ marginBottom: '8px' }} />
-            <Skeleton
-              variant="rect"
-              style={{ paddingTop: '80%' }}
-              width="auto"
-              height="auto"
-              animation="wave"
-            />
-          </Grid>
-        )}
-        <Grid item xs={12} sm={6} md={5} component={Paper} elevation={6} square>
-          {[...Array(33)].map((i) => (
-            <Skeleton animation="wave" key={i} style={{ marginTop: '8px' }} />
-          ))}
-        </Grid>
-      </Grid>
-    )
+    return <CharacterDetailSkeleton />
   }
   if (
     !isObjEmpty(character) &&
@@ -92,10 +50,7 @@ export default function CharacterDetail() {
     const characterInfo = character.results[0]
     return (
       <>
-        <CharacterDetailInfo
-          isExtraSmallScreen={isExtraSmallScreen}
-          characterInfo={characterInfo}
-        />
+        <CharacterDetailInfo characterInfo={characterInfo} />
       </>
     )
   }
