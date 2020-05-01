f5b9597bd7201ef61a74650a974c75686ecc717f
zomervinicius <vinicius.zomer123@gmail.com>
2020-05-01 08:43:57 -0300

    remove counter example

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../features/counter/counterSlice'
 import characterReducer from '../features/marvelCharacters/slices/CharacterSlice'
 import charactersReducer from '../features/marvelCharacters/slices/CharactersSlice'
 import paginationReducer from '../features/marvelCharacters/slices/PaginationSlice'
@@ -7,7 +6,6 @@ import searchReducer from '../features/marvelCharacters/slices/SearchSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
     characters: charactersReducer,
     character: characterReducer,
     pagination: paginationReducer,
